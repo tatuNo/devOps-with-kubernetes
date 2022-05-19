db6261a900e58ffda8ef76fabb57d3452e06f1dd
tatuNo <Tatu.Nordstrom@metropolia.fi>
2022-05-19 19:41:52 +0300

    exercise 1.05

--- part1/todo/index.js
+++ part1/todo/index.js
@@ -3,6 +3,11 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
+app.get('/', (_req, res) => {
+  res.send('<h2>Welcome to todos</h2>');
+});
+
+
 app.listen(PORT, () => {
   console.log(`Server started in port ${PORT}`);
 });
