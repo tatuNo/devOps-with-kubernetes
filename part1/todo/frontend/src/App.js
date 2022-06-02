c9a66a5c358801695436988c9221a28bfd49a659
tatuNo <Tatu.Nordstrom@metropolia.fi>
2022-06-02 12:56:36 +0300

    Exercise 1.13

--- part1/todo/frontend/src/App.js
+++ part1/todo/frontend/src/App.js
@@ -1,9 +1,31 @@
+import { useState } from 'react';
+
+const todos = [
+  'TODO1', 
+  'TODO2',
+  'TODO3'
+];
 
 const App = () => {
-  
+  const [todo, setTodo] = useState('');
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('handle submit');
+  }
   return(
     <div>
       <img src='http://localhost:8081/api' alt="" />
+      <form onSubmit={handleSubmit}>
+        <input type="text" maxLength="140" value={todo} onChange={(e) => setTodo(e.target.value)}/>
+        <input type="submit" value="Create TODO" />
+      </form>
+      <ul>
+        {todos.map(todo =>
+          <li key={todo}>
+            {todo}
+          </li> 
+        )}
+      </ul>
     </div>
   );
 };
